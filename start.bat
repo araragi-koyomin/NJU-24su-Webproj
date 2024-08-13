dbce600db73f28922b09f6502a50fd5aee0acbbf
araraki koyomin <3099146950@qq.com>
2024-08-13 22:29:00 +0800

    README

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Start the backend
-cd backend
-start /B node dist/bootstrap.js
-timeout /t 5 /nobreak
-
-REM Start a simple HTTP server for the frontend
-cd ..\frontend\dist
-start npx http-server -p 4173
